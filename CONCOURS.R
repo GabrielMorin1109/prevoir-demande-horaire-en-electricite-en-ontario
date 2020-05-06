59862b44cb54c6d2966424cfd7fced5ef581c56a
Gab1109 <gabriel.morin1109@outlook.com>
2020-05-06 13:42:18 -0400

    read.csav fonctionne pas

--- CONCOURS.R
+++ CONCOURS.R
@@ -1,26 +1,10 @@
+options(java.parameters = "-Xmx8000m") #afin de donner plus de heap space a java
+library(tidyverse)
+library(readxl)
+library(xlsx)
 
-library(openxlsx)
-library(usethis)
-# install.packages("openxlsx", dependencies = TRUE)
-
-allo test bonjour
-print(testing)
-
-print(testings)
-
-
-library(tmaptools)
-
-jhgkads
-
-
-# Saluuuut - Mathilde
-
-asdfghj
-salut
-
-
-print("hello world from X")
-
-
-getwd()
+# test <- read.csv(paste0(getwd(),"/Database/hourly_demand_MANUELLEMENT.csv"), encoding="UTF-8")
+test2 <- read.csv("/home/gabriel/Github/prevoir-demande-horaire-en-electricite-en-ontario/Database/hourly_demand.csv", encoding = "UTF-8", sep = ";")
+test.wth <- read.csv("/home/gabriel/Github/prevoir-demande-horaire-en-electricite-en-ontario/Database/hourly_weather.csv", encoding = "UTF-8", sep = ";", header = T)
+test %>% head()
+test2 %>% head()
