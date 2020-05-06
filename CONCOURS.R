9701a175dc22ba9fade3858d696b0949d0f9f64d
Mathilde7 <mathilde.bourget@hotmail.com>
2020-05-06 11:14:20 -0400

    11h14

--- CONCOURS.R
+++ CONCOURS.R
@@ -2,12 +2,11 @@
 library(openxlsx)
 library(usethis)
 # install.packages("openxlsx", dependencies = TRUE)
-<<<<<<< HEAD
+
 allo test bonjour
 print(testing)
-=======
-print(testings)
->>>>>>> 638066ab6cf616db8c5a1688659f2e9874cba5cb
+
+
 
 library(tmaptools)
 
