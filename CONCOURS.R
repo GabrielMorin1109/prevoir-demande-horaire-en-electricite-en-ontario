5120ebea60eb495fb394cab97c306b9b1d6f5336
Gab1109 <gabriel.morin1109@outlook.com>
2020-05-05 20:31:58 -0400

    Test

--- CONCOURS.R
+++ CONCOURS.R
@@ -1,4 +1,10 @@
 
 library(openxlsx)
+library(usethis)
 # install.packages("openxlsx", dependencies = TRUE)
 print(testing)
+894a073bb70b65cca258055a53a2e8b476c8c85f
+
+
+as
+ssh://github.com/GabrielMorin1109/prevoir-demande-horaire-en-electricite-en-ontario
